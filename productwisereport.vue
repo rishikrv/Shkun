025ce6cea8339aed68f2f8051c1dabf4a0ffb82d
79-creator <61737172+79-creator@users.noreply.github.com>
2020-03-08 17:18:47 +0530

    shreyachanyana

---
+++ productwisereport.vue
@@ -0,0 +1,494 @@
+/* eslint-disable vue/valid-template-root */
+<style>
+.overlap
+{
+    max-width: 150px;
+    max-height: 150px;
+    overflow: hidden;
+}
+</style>
+<style>
+#page-wraps {
+   
+    width: 100%;
+  
+    position: relative;
+    
+
+}
+
+#lefts {
+    
+    width: 50%;
+    height: auto;
+   
+    float: left;
+    padding-right: 9%;
+    
+    
+}
+
+#rights {
+    width: 50%;
+    height: auto;
+    float: left;
+   
+    padding-left: 9%;
+  
+}
+
+</style>
+<style>
+#page-wrap {
+   
+    width: 100%;
+    margin: 40px auto;
+    position: relative;
+    
+
+}
+
+#left {
+    
+    width: 33%;
+    height: auto;
+   
+    float: left;
+    padding-right: 8%;
+    
+    
+}
+
+#mid {
+    width: 33%;
+    height: auto;
+    float: left;
+   
+    padding-left: 8%;
+  
+}
+
+#rightt {
+    
+    width: 34%;
+    height: auto;
+    float: left;
+    padding-right: 2.5%;
+    padding-left: 2.5%;
+}
+</style>
+<style>
+
+.selectp
+{
+  border-color: #7367f0;
+  background-color:White;
+}
+
+.btn {
+  background-color: #7367f0;
+  border: none;
+  color: white;
+  padding: 12px 16px;
+  font-size: 16px;
+  
+  cursor: pointer;
+  border-radius: 12px;
+
+
+
+
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: RoyalBlue;
+}
+</style>
+
+<style>
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #7367f0;
+  color: white;
+}
+</style>
+<style>
+
+.right {
+  position: static;
+  right: 0px;
+  
+  padding: 10px;
+  
+   width: 100%;
+   min-width: 10px;  
+   max-width: 300px;
+
+}
+</style>
+
+<style type="text/css">
+
+
+@media screen and (min-width: 601px) {
+  div.example {
+    font-size: 15px;
+    background-color:white; 
+    padding: 55px;
+    
+    border-color: #7367f0;
+     border-top-style: solid;
+     border-bottom-style: solid;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  div.example {
+    font-size: 10px;
+    background-color:white; 
+    padding: 10px;
+    
+    border-color: #7367f0;
+    border-top-style: solid;
+    border-bottom-style: solid;
+  }
+  
+}
+
+.btns {
+   width: 100%;
+   min-width: 10px;  
+   max-width: 300px; 
+}
+</style>
+<style type="text/css">
+  #middlecol {
+   
+    width: 100%;
+    
+  
+  }
+
+ 
+  .tables 
+{
+    table-layout:fixed;
+    width:100%;
+    
+}
+
+.users {
+  table-layout: fixed;
+  white-space: nowrap;
+}
+.users td {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+/* Column widths are based on these cells */
+.rowid {
+  width: 4%;
+  
+   
+
+}
+</style>
+
+
+
+<template>
+
+    <div class="vx-card p-6" style="" >
+     <!--UPDATE START-->
+      <table width="100%" border="0" class="tables">
+     <tr>
+      <td width="25%">
+        
+      </td>
+       <td width="50%"><center><h1>Product Wise Report<br><h4><font color="blue">Purchase</font></h4></h1></center></td>
+       <td width="25%"  align="right"></td>
+     </tr></table>
+
+
+
+<!--UPDATE END-->
+  
+    <table border="0" width="100%" class="tables" >
+      
+        <tr>
+            <td width="30%" align="left" >
+                <table border="0" class="tables" align="left" cellspacing="5">
+                <tr>
+                <td>
+               From
+                </td>
+                <td >
+                 <vs-input type="date" class="w-full"  size="small" v-model="mydate"/>
+                </td>
+                </tr>
+                
+                <tr>
+                <td>
+               Upto
+                </td>
+                <td >
+                 <vs-input type="date" class="w-full"  size="small" v-model="mydate"/>
+                </td>
+                </tr>
+    
+                </table>
+            </td>
+            
+           
+            <td width="20%"></td>
+        <td width="50%"  align="right">
+           
+                
+              <div align="right">
+       <div class="right" align="right">
+        <td width="25%"  align="right">Today 25-10-2020 <br> Thursday</td>
+        
+         </div>
+        </div>
+            </td>
+        </tr>
+        
+    </table>
+
+
+   
+<br>
+
+<tr>
+                <td style="padding-left:0px">
+                <table border="0" width="100%" cellspacing="5"  class="tables" >
+                <td width="50%">
+                <vs-button class="w-full" size="small">Select Ledger Account</vs-button>
+                </td>
+                 
+               
+                </table>
+                </td>
+
+                <td style="padding-left:0px">
+                <table border="0" width="100%" cellspacing="5"  class="tables" >
+                <td width="50%">
+                <vs-button class="w-full"  size="small">Select product</vs-button>
+                </td>
+                 
+               
+                </table>
+                </td>
+        </tr>
+
+
+
+
+                 
+
+
+
+
+
+
+
+
+
+
+<div id="page-wrap">
+    <div id="lefts">
+
+    <table border="0"  cellspacing="5" width="100%" class="tables">
+       <tr>
+        
+         <td align="center" style="padding-left:20px"><h4><font color="7367f0">Account Options</font></h4></td>
+      </tr>  
+      <br>
+
+
+      <tr>
+        <td class="div.example">City Name</td>
+         <td  style="padding-left:20px"><vs-input class="w-full" size="small" v-model="input1" /></td>
+      </tr>
+      <tr>
+        <td class="div.example">State</td>
+        <td  style="padding-left:20px"><vs-input class="w-full" size="small" v-model="input1" /></td>
+      </tr>
+      <tr>
+
+          <td  style="padding-left:0px">
+            
+            
+          <table border="1" width="100%" cellspacing="0"  class="tables">
+        
+        <tr>
+          <td width="95%" class="div.example" >Agent B2B</td>
+           <input width="5%"  align ="right" type="checkbox"  class="mr-3 mb-2" id="vehicle1" name="vehicle1" value="Bike">
+        </tr>
+      </table>
+    
+     <td  style="padding-left:20px">
+            
+            
+          <table border="0" width="100%" cellspacing="0"  class="tables">
+        
+        <tr>
+          <td width="75%"  style="padding-left: 0px"><vs-input class="w-full" size="small" v-model="input1" /></td>
+          <td align="right" width="25%" style="padding-left: 15px;padding-top: 5px;">
+           <vs-button style="padding-left: 10px" class="mr-3 mb-2" size="small">...</vs-button>
+          </td>
+        </tr>
+      </table>
+            
+            </td>
+
+
+      </tr>
+
+    <tr>
+        <td class="div.example">Full Adress</td>
+        <td  style="padding-left:20px"><select class="w-full selectp">
+        <option value="">No</option>
+        </select></td>
+      </tr>
+
+
+      <tr>
+        <td class="div.example" >Rate Colm.</td>
+    
+        <td  style="padding-left:20px">
+            
+            
+          <table border="0" width="100%" cellspacing="0"  class="tables">
+        
+        <tr>
+          <td  style="padding-left: 0px"><select class="w-full selectp">
+        <option value="">No</option>
+        </select></td>
+          <td  style="padding-left: 0px"><vs-input class="w-full"  size="small" v-model="input1" /></td>
+          <td  style="padding-left: 0px"><vs-input class="w-full"  size="small" v-model="input1" /></td>
+        </tr>
+      </table>
+            
+            </td>
+      </tr>
+
+
+          <tr>
+        <td class="div.example">Series</td>
+        <td  style="padding-left:20px"><select class="w-full selectp">
+        <option value=""></option>
+        </select></td>
+      </tr>
+
+         <tr>
+        <td class="div.example">Group</td>
+         <td  style="padding-left:20px"><vs-input class="w-full" size="small" v-model="input1" /></td>
+      </tr>
+
+
+       
+      
+
+    </table>
+    </div>
+    <div id="rights">
+      <table border="0" width="100%" cellspacing="5"  class="tables" >
+
+          <tr>
+        
+         <td align="center" style="padding-left:20px"><h4><font color="7367f0">Product Options</font></h4></td>
+      </tr>  
+      <br>
+
+       <tr>
+        <td class="div.example">
+            <vs-radio style="padding-right: 5px;" v-model="radios1" vs-value="luis">Gross</vs-radio>
+            <vs-radio style="padding-right: 5px;" v-model="radios1" vs-value="luis">Detailed</vs-radio>
+            <vs-radio v-model="radios1" vs-value="luis">Group</vs-radio>
+   
+     </td>
+      </tr>
+   
+     
+          <tr>
+        <td class="div.example">Order By</td>
+        <td  style="padding-left:20px"><select class="w-full selectp">
+        <option value=""></option>
+        </select></td>
+      </tr>
+      <tr>
+        <td class="div.example">Description</td>
+       <td  style="padding-left:20px"><vs-input class="w-full" size="small" v-model="input1" /></td>
+      </tr>
+      <tr>
+        <td class="div.example">Qty Exceeding</td>
+        <td  style="padding-left:20px"><vs-input class="w-full" size="small" v-model="input1" /></td>
+      </tr>
+            
+     
+    </table>
+    </div>
+    </div>
+
+
+
+
+    
+    
+        <!--IMPORT THE SCRIPT FOR ADDING ROWS AND USE THIS BUTTON-->
+      <!--<button v-on:click="add_row">Add Row</button>-->
+      
+
+
+
+                <table border="0"  width="100%" >
+      
+        <tr align="right">
+            <td width="100%">
+                <table border="0">
+                <tr>
+                <td>
+                <vs-button class="mr-3 mb-2" style= "width:80px" size="small">Print</vs-button>
+                </td>
+                <td>
+                <vs-button class="mr-3 mb-2" style= "width:80px" size="small">Submit</vs-button>
+                </td>
+                <td>
+                <vs-button class="mr-3 mb-2" style= "width:80px" size="small">EXit</vs-button>
+                </td>
+               
+
+                </tr>
+                </table>
+                </td>
+                </tr>
+                </table>
+
+
+
+
+
+    </div>
+</template>
